8574b637d8d42658002edf3a814c66eb4074b027
Brian Bycynski <high5bri@hotmail.com>
2018-04-16 12:41:11 -0400

    finished lecture 140

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -27,7 +27,7 @@ const formatCount = count => {
 	return "?";
 };
 
-export const createIngredient = ingredient => `
+const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
@@ -65,12 +65,12 @@ export const renderRecipe = recipe => {
         <span class="recipe__info-text"> servings</span>
 
         <div class="recipe__info-buttons">
-            <button class="btn-tiny">
+            <button class="btn-tiny btn-decrease">
                 <svg>
                     <use href="img/icons.svg#icon-circle-with-minus"></use>
                 </svg>
             </button>
-            <button class="btn-tiny">
+            <button class="btn-tiny btn-increase">
                 <svg>
                     <use href="img/icons.svg#icon-circle-with-plus"></use>
                 </svg>
@@ -123,4 +123,17 @@ export const renderRecipe = recipe => {
 </div>
     `;
 	elements.recipe.insertAdjacentHTML("afterbegin", markup);
+};
+
+export const updateServingsIngredients = recipe => {
+	// Update servings
+	document.querySelector(".recipe__info-data--people").textContent =
+		recipe.servings;
+	// Update Ingredients
+	const countElements = Array.from(
+		document.querySelectorAll(".recipe__count")
+	);
+	countElements.forEach((el, i) => {
+		el.textContent = formatCount(recipe.ingredients[i].count);
+	});
 };
